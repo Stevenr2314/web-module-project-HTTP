17474bf35f5ded947cc125649561754511b29045
Steven Rosa <s.rosa21@icloud.com>
2021-12-15 19:23:19 -0500

    commit

--- src/App.js
+++ src/App.js
@@ -9,13 +9,14 @@ import MovieHeader from './components/MovieHeader';
 import FavoriteMovieList from './components/FavoriteMovieList';
 
 import axios from 'axios';
+import EditMovieForm from "./components/EditMovieForm";
 
 const App = (props) => {
   const [movies, setMovies] = useState([]);
   const [favoriteMovies, setFavoriteMovies] = useState([]);
 
   useEffect(()=>{
-    axios.get('http://localhost:5001/api/movies')
+    axios.get('http://localhost:9000/api/movies')
       .then(res => {
         setMovies(res.data);
       })
@@ -25,6 +26,8 @@ const App = (props) => {
   }, []);
 
   const deleteMovie = (id)=> {
+    const updatedMovies = movies.filter(movie => movie.id != id)
+    setMovies(updatedMovies)
   }
 
   const addToFavorites = (movie) => {
@@ -44,14 +47,15 @@ const App = (props) => {
         
           <Switch>
             <Route path="/movies/edit/:id">
+              <EditMovieForm setMovies={setMovies}/>
             </Route>
 
             <Route path="/movies/:id">
-              <Movie/>
+              <Movie deleteMovie={deleteMovie} />
             </Route>
 
             <Route path="/movies">
-              <MovieList movies={movies}/>
+              <MovieList movies={movies} />
             </Route>
 
             <Route path="/">
--- src/components/DeleteMovieModal.js
+++ src/components/DeleteMovieModal.js
@@ -1,6 +1,18 @@
 import React from 'react';
+import axios from 'axios';
+import { useHistory } from 'react-router-dom';
 
-const DeleteMovieModal = () => {
+const DeleteMovieModal = props => {
+    const { push } = useHistory()
+    const handleDelete = e =>{
+        axios.delete(`http://localhost:5000/api/movie/${props.id}`)
+        .then(resp => {
+            console.log(resp)
+            props.deleteMovie(props.id)
+            push('/movies')
+        })
+        .catch(err => console.log(err))
+    }
     return (<div id="deleteEmployeeModal">
         <div className="modal-dialog">
             <div className="modal-content">
@@ -15,7 +27,7 @@ const DeleteMovieModal = () => {
                     </div>
                     <div className="modal-footer">
                         <input type="button" className="btn btn-default" data-dismiss="modal" value="Cancel"/>
-                        <input type="submit" className="btn btn-danger" value="Delete"/>
+                        <input type="submit" className="btn btn-danger" value="Delete" omClick={handleDelete}/>
                     </div>
                 </form>
             </div>
